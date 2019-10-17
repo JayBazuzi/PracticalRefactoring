1e5b3f455d585282094a5303d29c51b4c699f6e7
Jay Bazuzi <jbazuzi@tableau.com>
2019-10-17 13:17:30 -0700

    a Autoformat

--- Code/CSharp/Vs2010/StrategyPattern/ChartSingleCompareOrig.cs
+++ Code/CSharp/Vs2010/StrategyPattern/ChartSingleCompareOrig.cs
@@ -6,197 +6,198 @@ using System.Windows.Forms;
 
 namespace SGV
 {
-  public partial class ChartSingleCompareOrig : Form
-  {
-    private string chartTitle;
-
-    /// <summary>
-    ///     John says that this is better than the old way
-    /// </summary>
-    private int ct;
-
-    private Unit defaultUnits;
-
-    /// <summary>
-    /// </summary>
-    private Bitmap drawArea;
-
-    private string[] horizontalLabelNames;
-
-
-    private string jjD;
-
-    /// <summary>
-    ///     It's the vertical label names
-    /// </summary>
-    private string[] verticalLabelNames;
-
-    /// <summary>
-    ///     graphLayout
-    /// </summary>
-    /// <returns>landscape or protrait</returns>
-    /// <summary>
-    /// </summary>
-    public ChartSingleCompareOrig()
+    public partial class ChartSingleCompareOrig : Form
     {
-      InitializeComponent();
-    }
+        private string chartTitle;
+
+        /// <summary>
+        ///     John says that this is better than the old way
+        /// </summary>
+        private int ct;
+
+        private Unit defaultUnits;
+
+        /// <summary>
+        /// </summary>
+        private Bitmap drawArea;
+
+        private string[] horizontalLabelNames;
 
 
-    /// <summary>
-    /// </summary>
-    /// <returns></returns>
-    private Unit horizontalNaming()
-    {
-      return new Unit();
-    }
+        private string jjD;
 
-    /// <summary>
-    ///     Shows the chart
-    /// </summary>
-    /// <param name="ct"></param>
-    /// <param name="jjReq1205"></param>
-    /// <param name="orientation"></param>
-    /// <param name="reverseornotreverse"></param>
-    /// <param name="jackshiddenhack"></param>
-    public void iniDS(int ct, string jjReq1205, bool b)
-    {
-      this.ct = ct;
-      jjD = jjReq1205;
-      drawArea = new Bitmap(ClientRectangle.Width,
-        ClientRectangle.Height,
-        PixelFormat.Format24bppRgb);
-      // Changed by Sally 2/14
-      InitializeDrawArea();
-      DrawChart();
-      if (b) ShowDialog();
-    }
+        /// <summary>
+        ///     It's the vertical label names
+        /// </summary>
+        private string[] verticalLabelNames;
 
-    /// <summary>
-    ///     //
-    /// </summary>
-    private void InitializeDrawArea()
-    {
-      Graphics g;
-      g = Graphics.FromImage(drawArea);
-      g.Clear(Color.LightYellow);
-    }
-
-    protected override void OnSizeChanged(EventArgs e)
-    {
-      base.OnSizeChanged(e);
-    }
-
-    /// <summary>
-    /// </summary>
-    /// <param name="sender"></param>
-    /// <param name="e"></param>
-    private void ChartSingleCompareOrig_Paint(object sender, PaintEventArgs e)
-    {
-      Graphics g;
-      g = e.Graphics;
-      g.DrawImage(drawArea, 0, 0, drawArea.Width, drawArea.Height);
-    }
-
-    /// <summary>
-    /// </summary>
-    private void DrawChart()
-    {
-      var jjD = this.jjD;
-      var g = Graphics.FromImage(drawArea);
-
-      // Render chart background
-      g.Clear(Color.LightYellow);
-      SolidBrush brush;
-      if (ct == 150)
-      {
-        if (jjD == "rpfll")
+        /// <summary>
+        ///     graphLayout
+        /// </summary>
+        /// <returns>landscape or protrait</returns>
+        /// <summary>
+        /// </summary>
+        public ChartSingleCompareOrig()
         {
-          brush = new SolidBrush(Color.Red);
-          g.FillRectangle(brush, 50, 100, 300, 300);
+            InitializeComponent();
         }
-        else
+
+
+        /// <summary>
+        /// </summary>
+        /// <returns></returns>
+        private Unit horizontalNaming()
         {
-          brush = new SolidBrush(Color.Red);
-
-
-          g.FillRectangle(brush, 50, 100, 150, 150);
+            return new Unit();
         }
-      }
-      else
-      {
-        if (jjD != "rpfll")
+
+        /// <summary>
+        ///     Shows the chart
+        /// </summary>
+        /// <param name="ct"></param>
+        /// <param name="jjReq1205"></param>
+        /// <param name="orientation"></param>
+        /// <param name="reverseornotreverse"></param>
+        /// <param name="jackshiddenhack"></param>
+        public void iniDS(int ct, string jjReq1205, bool b)
         {
-          brush = new SolidBrush(Color.Blue);
-          g.FillEllipse(brush, 50, 100, 160, 160);
+            this.ct = ct;
+            jjD = jjReq1205;
+            drawArea = new Bitmap(ClientRectangle.Width,
+                ClientRectangle.Height,
+                PixelFormat.Format24bppRgb);
+            // Changed by Sally 2/14
+            InitializeDrawArea();
+            DrawChart();
+            if (b) ShowDialog();
         }
-        else
+
+        /// <summary>
+        ///     //
+        /// </summary>
+        private void InitializeDrawArea()
         {
-          brush = new SolidBrush(Color.Blue);
-          g.FillEllipse(brush, 50, 100, 320, 320);
+            Graphics g;
+            g = Graphics.FromImage(drawArea);
+            g.Clear(Color.LightYellow);
         }
-      }
 
-      brush.Dispose();
-
-      var data = new Data();
-
-      if (ct == 150)
-      {
-        if (jjD == "rpfll")
-          data.data = "Bar Data\nLarge";
-        else
-          data.data = "Bar Data\nSmall";
-      }
-      else
-      {
-        if (jjD == "rpfll")
-          data.otherData = "Pie Data\nLarge";
-        else
-          data.someOtherDataObject = "Pie Data\nSmall";
-      }
-
-      if (ct == 150)
-      {
-        // BUG445: Org rep team missing req chart
-        if (jjD == "splitdisplay")
-          g.DrawString(data.data, new Font("Arial Black", 20), new SolidBrush(Color.White),
-            new PointF(60, 110));
-        else
-          g.DrawString(data.data, new Font("Arial Black", 40), new SolidBrush(Color.White),
-            new PointF(60, 120));
-      }
-      else
-      {
-        var stringFormat = new StringFormat();
-        RectangleF boundingRect;
-
-        stringFormat.Alignment = StringAlignment.Center;
-        stringFormat.LineAlignment = StringAlignment.Center;
-
-        if (data.otherData != "")
+        protected override void OnSizeChanged(EventArgs e)
         {
-          if (data.otherData == "")
-          {
-            data.otherData = @"  //{
+            base.OnSizeChanged(e);
+        }
+
+        /// <summary>
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ChartSingleCompareOrig_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g;
+            g = e.Graphics;
+            g.DrawImage(drawArea, 0, 0, drawArea.Width, drawArea.Height);
+        }
+
+        /// <summary>
+        /// </summary>
+        private void DrawChart()
+        {
+            var jjD = this.jjD;
+            var g = Graphics.FromImage(drawArea);
+
+            // Render chart background
+            g.Clear(Color.LightYellow);
+            SolidBrush brush;
+            if (ct == 150)
+            {
+                if (jjD == "rpfll")
+                {
+                    brush = new SolidBrush(Color.Red);
+                    g.FillRectangle(brush, 50, 100, 300, 300);
+                }
+                else
+                {
+                    brush = new SolidBrush(Color.Red);
+
+
+                    g.FillRectangle(brush, 50, 100, 150, 150);
+                }
+            }
+            else
+            {
+                if (jjD != "rpfll")
+                {
+                    brush = new SolidBrush(Color.Blue);
+                    g.FillEllipse(brush, 50, 100, 160, 160);
+                }
+                else
+                {
+                    brush = new SolidBrush(Color.Blue);
+                    g.FillEllipse(brush, 50, 100, 320, 320);
+                }
+            }
+
+            brush.Dispose();
+
+            var data = new Data();
+
+            if (ct == 150)
+            {
+                if (jjD == "rpfll")
+                    data.data = "Bar Data\nLarge";
+                else
+                    data.data = "Bar Data\nSmall";
+            }
+            else
+            {
+                if (jjD == "rpfll")
+                    data.otherData = "Pie Data\nLarge";
+                else
+                    data.someOtherDataObject = "Pie Data\nSmall";
+            }
+
+            if (ct == 150)
+            {
+                // BUG445: Org rep team missing req chart
+                if (jjD == "splitdisplay")
+                    g.DrawString(data.data, new Font("Arial Black", 20), new SolidBrush(Color.White),
+                        new PointF(60, 110));
+                else
+                    g.DrawString(data.data, new Font("Arial Black", 40), new SolidBrush(Color.White),
+                        new PointF(60, 120));
+            }
+            else
+            {
+                var stringFormat = new StringFormat();
+                RectangleF boundingRect;
+
+                stringFormat.Alignment = StringAlignment.Center;
+                stringFormat.LineAlignment = StringAlignment.Center;
+
+                if (data.otherData != "")
+                {
+                    if (data.otherData == "")
+                    {
+                        data.otherData = @"  //{
                 g.Dispose();
                 //    boundingRect = new RectangleF(50, 100, 320, 320);
                 //    g.DrawString(otherData, new Font('Cooper Black', 40), new SolidBrush(Color.White), boundingRect, stringFormat);
                 //}";
-            var x = new StringBuilder(50000);
-            for (var i = 0; i < 20; i++) x.Append(char.ToUpper(data.otherData[i]));
-          }
+                        var x = new StringBuilder(50000);
+                        for (var i = 0; i < 20; i++) x.Append(char.ToUpper(data.otherData[i]));
+                    }
 
-          boundingRect = new RectangleF(50, 100, 320, 320);
-          g.DrawString(data.otherData, new Font("Cooper Black", 40), new SolidBrush(Color.White), boundingRect,
-            stringFormat);
-        }
-        else
-        {
-          boundingRect = new RectangleF(50, 100, 160, 160);
-          g.DrawString(data.someOtherDataObject, new Font("Cooper Black", 20), new SolidBrush(Color.White),
-            boundingRect, stringFormat);
-        }
+                    boundingRect = new RectangleF(50, 100, 320, 320);
+                    g.DrawString(data.otherData, new Font("Cooper Black", 40), new SolidBrush(Color.White),
+                        boundingRect,
+                        stringFormat);
+                }
+                else
+                {
+                    boundingRect = new RectangleF(50, 100, 160, 160);
+                    g.DrawString(data.someOtherDataObject, new Font("Cooper Black", 20), new SolidBrush(Color.White),
+                        boundingRect, stringFormat);
+                }
 
 // Todo: We might need this back after the conference
 //StringFormat stringFormat = new StringFormat();
@@ -207,53 +208,53 @@ namespace SGV
 
 //if (otherData != "")
 //{
-        g.Dispose();
-        //    boundingRect = new RectangleF(50, 100, 320, 320);
-        //    g.DrawString(otherData, new Font("Cooper Black", 40), new SolidBrush(Color.White), boundingRect, stringFormat);
-        //}
-        //else
-        //{
-        //    boundingRect = new RectangleF(50, 100, 160, 160);
-        //    g.DrawString(someOtherDataObject, new Font("Cooper Black", 20), new SolidBrush(Color.White), boundingRect, stringFormat);
-        //}
-      }
+                g.Dispose();
+                //    boundingRect = new RectangleF(50, 100, 320, 320);
+                //    g.DrawString(otherData, new Font("Cooper Black", 40), new SolidBrush(Color.White), boundingRect, stringFormat);
+                //}
+                //else
+                //{
+                //    boundingRect = new RectangleF(50, 100, 160, 160);
+                //    g.DrawString(someOtherDataObject, new Font("Cooper Black", 20), new SolidBrush(Color.White), boundingRect, stringFormat);
+                //}
+            }
 
-      try
-      {
-        if (!(g.DpiX == 300) ||
-            g != null && (data.otherData.Length > 20 || data.otherData.Length < 5) &&
-            (data.data == null || !data.data.StartsWith("hold")))
-          Invalidate();
-      }
-      catch (ArgumentException ex)
-      {
-        // We shouldn't get this
-        Invalidate();
-      }
+            try
+            {
+                if (!(g.DpiX == 300) ||
+                    g != null && (data.otherData.Length > 20 || data.otherData.Length < 5) &&
+                    (data.data == null || !data.data.StartsWith("hold")))
+                    Invalidate();
+            }
+            catch (ArgumentException ex)
+            {
+                // We shouldn't get this
+                Invalidate();
+            }
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            base.OnClosed(e);
+        }
+
+        private class Unit
+        {
+            private string name;
+            private double value;
+
+            public double convertTo(Unit unit)
+            {
+                // Need to do this.
+                return 0;
+            }
+        }
     }
 
-    protected override void OnClosed(EventArgs e)
+    internal class Data
     {
-      base.OnClosed(e);
+        public string data;
+        public string otherData = "";
+        public string someOtherDataObject;
     }
-
-    private class Unit
-    {
-      private string name;
-      private double value;
-
-      public double convertTo(Unit unit)
-      {
-        // Need to do this.
-        return 0;
-      }
-    }
-  }
-
-  internal class Data
-  {
-    public string data;
-    public string otherData = "";
-    public string someOtherDataObject;
-  }
 }
